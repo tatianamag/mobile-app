0bddecb7de450232685be1a7f6a3feab34fb5f03
Tu Nombre <delfiavalos35@gmail.com>
2025-07-02 00:23:53 -0300

    creación de RetrofitInstance

---
+++ app/src/main/java/com/novacenter/app/data/network/RetrofitInstance.kt
@@ -0,0 +1,28 @@
+package com.novacenter.app.data.network
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitInstance {
+    private const val BASE_URL = "https://tu-servidor/api/" // reemplazalo con tu endpoint real
+
+    val retrofit: Retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    val turnoService: TurnoService by lazy {
+        retrofit.create(TurnoService::class.java)
+    }
+
+    val authService: AuthService by lazy {
+        retrofit.create(AuthService::class.java)
+    }
+
+    val usuarioService: UsuarioService by lazy {
+        retrofit.create(UsuarioService::class.java)
+    }
+
+}
